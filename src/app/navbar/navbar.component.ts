b968e09e917141559b6212c06f7e83e39d83147f
Gautamp11 <gautam1564p1@gmail.com>
2024-04-14 14:03:33 +0530

    responsive-nav
    made the nav responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     // Select all elements with class 'nav-link'
     const links = document.querySelectorAll('.nav-link');
 
     // For each link, attach a click event listener
-    links.forEach(link => {
+    links.forEach((link) => {
       link.addEventListener('click', () => {
         // Remove 'active' class from all links
-        links.forEach(l => l.classList.remove('active'));
+        links.forEach((l) => l.classList.remove('active'));
 
         // Add 'active' class to the clicked link
         link.classList.add('active');
       });
     });
+  }
+
+  toggleNav() {
+    document.querySelector('.menu')?.classList.toggle('show');
+    document.querySelector('.nav')?.classList.toggle('show');
   }
 }
